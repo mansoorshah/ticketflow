27585e6e760fe86a11fafbfe9aae8501a291d616
Mansoor Shah <mansoorshah@live.com>
2025-12-28 20:29:52 +0500

    Update config.php
    Commented decrypted text to replace sensitive info when deploying. Decryption will occur using secret key.

--- config/config.php
+++ config/config.php
@@ -11,7 +11,7 @@ define('MAIL_ENCRYPTION', 'tls');
 
 define('MAIL_USERNAME', 'Enter your email here with @');
 define('MAIL_PASSWORD', 'enter your API key here');
-
+/*Decrypt this text with secret key replace above: pk8w1apg65Doneu1DrG4Bzg9HH7eYBCJKsYkn3oUi72LyHswr1CH8eCCLbwpv0JG+LwHkA72Bwx6hE0McbpuAEnuiqK19ZXuCpcR2PDiCs8F0ro/yBopKiGKYWQItw1QzNWwslhzdoQopCc30/xWjoGDAcnPQ0nbeSfRaEEMPLjy1Zscf9fmGqQ8K3c9xJHq */
 /*define('MAIL_FROM_EMAIL', 'MS_YD4iIa@test-r9084zv1oy8gw63d.mlsender.net');
 define('MAIL_FROM_NAME', 'TicketFlow');*/
 
@@ -19,5 +19,6 @@ define('MAILERSEND_API_TOKEN', 'mlsn.f4f27385d61ad3df1eb4f626c3412f2be4909b8de8e
 
 define('MAIL_FROM_EMAIL', 'no-reply@test-r9084zv1oy8gw63d.mlsender.net');
 define('MAIL_FROM_NAME', 'TicketFlow');
+
 
 
