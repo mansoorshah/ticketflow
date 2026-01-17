03187c76a7021eb17666c5af419d05f50e45ced2
Mansoor Shah <mansoorshah@live.com>
2026-01-18 00:13:54 +0500

    Removed MAILERSEND API token from config
    Removed sensitive MAILERSEND API token from config.

--- config/config.php
+++ config/config.php
@@ -27,10 +27,11 @@ define('MAIL_PASSWORD', 'mssp.lxsyY6y.jy7zpl9pn95g5vx6.FtNTNPH');
 // ==================
 // MAILERSEND API
 // ==================
-define('MAILERSEND_API_TOKEN', 'mlsn.f4f27385d61ad3df1eb4f626c3412f2be4909b8de8e37998d231051a1bdabb2d');
+define('MAILERSEND_API_TOKEN', '');
 
 // ==================
 // FROM
 // ==================
 define('MAIL_FROM_EMAIL', 'no-reply@test-r9084zv1oy8gw63d.mlsender.net');
 define('MAIL_FROM_NAME', 'TicketFlow');
+
